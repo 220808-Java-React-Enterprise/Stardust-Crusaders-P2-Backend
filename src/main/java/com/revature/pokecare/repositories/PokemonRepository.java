ac7205fda976e9710271e78b0bf39befd7f6052a
Leighton Albrecht <leightonalbrecht@gmail.com>
2022-09-21 12:32:30 -0400

    updated authorization header label to user-auth

--- src/main/java/com/revature/pokecare/repositories/PokemonRepository.java
+++ src/main/java/com/revature/pokecare/repositories/PokemonRepository.java
@@ -1,9 +1,11 @@
 package com.revature.pokecare.repositories;
 
 import com.revature.pokecare.models.Pokemon;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +18,9 @@ public interface PokemonRepository extends CrudRepository<Pokemon, String> {
     @Query(value = "SELECT * FROM pokemon WHERE user_id = ?1", nativeQuery = true)
     List<String> findByUserID(String user_id);
 
+    @Query(value = "SELECT * FROM pokemon WHERE user_id = ?1 AND in_daycare = true", nativeQuery = true)
+    List<String> getFromDaycare(String user_id);
+
     @Query(value = "SELECT hpEV FROM pokemon WHERE pokemon_id = ?1", nativeQuery = true)
     int findHPEV(String pokemon_id);
 
@@ -24,5 +29,10 @@ public interface PokemonRepository extends CrudRepository<Pokemon, String> {
 
     @Query(value = "SELECT level FROM pokemon WHERE pokemon_id = ?1", nativeQuery = true)
     int findLevel(String pokemon_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE pokemon SET in_daycare = ?2 WHERE pokemon_id = ?1", nativeQuery = true)
+    void setDaycareStatus(String id, boolean isActive);
 
 }
